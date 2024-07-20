15c11b98828cb160dcc1a4da3722d1835d474bd4
EC2 Default User <ec2-user@ip-172-31-40-214.ap-southeast-1.compute.internal>
2024-07-20 21:04:43 +0800

    init project

---
+++ src/accessSheets.mjs
@@ -0,0 +1,75 @@
+import fs from 'fs';
+import { google } from 'googleapis';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+// Load the service account key JSON file
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+const KEYFILEPATH = '/home/ec2-user/cpbpc-bot/.jovial-pod-412005-8dbef88791a5.json';
+const SCOPES = ['https://www.googleapis.com/auth/spreadsheets'];
+
+// Authenticate with the Google Sheets API
+async function authenticate() {
+    const auth = new google.auth.GoogleAuth({
+        keyFile: KEYFILEPATH,
+        scopes: SCOPES,
+    });
+
+    const authClient = await auth.getClient();
+    return authClient;
+}
+
+// Read data from Google Sheets
+async function readSheet() {
+    const authClient = await authenticate();
+    const sheets = google.sheets({ version: 'v4', auth: authClient });
+
+    const spreadsheetId = 'XXX';
+    const range = '2024-08!A1:C10'; // Adjust the range as needed
+
+    const response = await sheets.spreadsheets.values.get({
+        spreadsheetId,
+        range,
+    });
+
+    const rows = response.data.values;
+    if (rows.length) {
+        console.log('Data from the spreadsheet:');
+        rows.map((row) => {
+            console.log(row);
+        });
+    } else {
+        console.log('No data found.');
+    }
+}
+
+// Write data to Google Sheets
+async function writeSheet() {
+    const authClient = await authenticate();
+    const sheets = google.sheets({ version: 'v4', auth: authClient });
+
+    const spreadsheetId = 'your-spreadsheet-id';
+    const range = 'Sheet1!A1'; // Adjust the range as needed
+    const valueInputOption = 'RAW';
+    const resource = {
+        values: [
+            ['Column A', 'Column B', 'Column C', 'Column D'],
+            ['Value 1', 'Value 2', 'Value 3', 'Value 4'],
+        ],
+    };
+
+    const response = await sheets.spreadsheets.values.update({
+        spreadsheetId,
+        range,
+        valueInputOption,
+        resource,
+    });
+
+    console.log(`${response.data.updatedCells} cells updated.`);
+}
+
+// Uncomment the function you want to use
+readSheet();
+// writeSheet();
+
---
+++ src/bot.mjs
@@ -0,0 +1,146 @@
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import fetch from 'node-fetch';
+import fs from 'fs';
+import properties from 'properties';
+
+// Replace 'YOUR_API_TOKEN' with your bot's API token
+let API_TOKEN = null;
+let TELEGRAM_API_URL = null;
+let WEBHOOK_URL = null;
+
+const app = express();
+app.use(bodyParser.json());
+
+app.post('/webhook', (req, res) => {
+    const update = req.body;
+    console.info(`message is ${JSON.stringify(update)}`);
+    if (update.message) {
+        handleMessage(update.message);
+    } else if (update.callback_query) {
+        handleCallbackQuery(update.callback_query);
+    }
+
+    res.sendStatus(200);
+});
+
+const handleMessage = async (message) => {
+    const chatId = message.chat.id;
+    const text = message.text;
+
+    if (text === '/start') {
+        const keyboard = {
+            inline_keyboard: [
+                [
+                    { text: 'Option 1', callback_data: '1' },
+                    { text: 'Option 2', callback_data: '2' }
+                ]
+            ]
+        };
+        await sendMessage(chatId, 'Please choose:', keyboard);
+    } else {
+        await sendMessage(chatId, `You said: ${text}`);
+    }
+};
+
+const handleCallbackQuery = async (callbackQuery) => {
+    const chatId = callbackQuery.message.chat.id;
+    const data = callbackQuery.data;
+
+    await sendMessage(chatId, `You selected option: ${data}`);
+};
+
+const sendMessage = async (chatId, text, replyMarkup = null) => {
+    const url = `${TELEGRAM_API_URL}sendMessage`;
+    console.info( `send message url is ${url}` );
+    let payload = null;
+    if ( replyMarkup ){
+        payload = {
+            chat_id: chatId,
+                text: text,
+            reply_markup: replyMarkup
+        };
+    }
+    if ( !replyMarkup ){
+        payload = {
+            chat_id: chatId,
+            text: text
+        };
+    }
+
+    // await fetch(url, {
+    //     method: 'POST',
+    //     headers: { 'Content-Type': 'application/json' },
+    //     body: JSON.stringify(payload)
+    // });
+
+    try {
+        console.info(`reply payload is ${text}`)
+        const response = await fetch(url, {
+            method: 'POST',
+            headers: { 'Content-Type': 'application/json' },
+            body: JSON.stringify(payload)
+        });
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}, ${JSON.stringify(response.body)}`);
+        }
+
+        const data = await response.json();
+        console.log('Response from server:', data);
+    } catch (error) {
+        console.error('Error fetching data:', error.message);
+    }
+};
+
+const readPropertiesAndSetToken = async (filePath) => {
+    try {
+        const data = fs.readFileSync(filePath, 'utf8');
+        const obj = properties.parse(data);
+        API_TOKEN = obj.telemgram_bot_api_key; // Assuming the key in properties is 'telemgram_bot_api_key'
+        TELEGRAM_API_URL = `https://api.telegram.org/bot${API_TOKEN}/`;
+        WEBHOOK_URL = `https://rpg-7f76ccf078caba5f.elb.ap-southeast-1.amazonaws.com:88/webhook`;
+
+        console.info( `TELEGRAM_API_URL ${TELEGRAM_API_URL}` );
+        console.info( `WEBHOOK_URL ${WEBHOOK_URL}` );
+    } catch (error) {
+        console.error('Error parsing properties:', error);
+    }
+};
+
+app.listen(89, async () => {
+    console.log('Bot server is running on port 89');
+    await readPropertiesAndSetToken('../.secret');
+    // checkAndSetWebhook();
+});
+
+app.get('/health', (req, res) => {
+    res.status(200).send('Server is healthy');
+});
+const checkAndSetWebhook = async () => {
+    try {
+        console.info(`telegram api url : ${TELEGRAM_API_URL}getWebhookInfo`)
+        const getWebhookInfoUrl = `${TELEGRAM_API_URL}getWebhookInfo`;
+        const response = await fetch(getWebhookInfoUrl);
+        const data = await response.json();
+
+        if (data.result.url !== WEBHOOK_URL) {
+            console.log('Setting webhook...');
+            const setWebhookUrl = `${TELEGRAM_API_URL}setWebhook?url=${WEBHOOK_URL}`;
+            const setResponse = await fetch(setWebhookUrl);
+            const setData = await setResponse.json();
+
+            if (setData.ok) {
+                console.log('Webhook set successfully');
+            } else {
+                console.error('Error setting webhook:', setData);
+            }
+        } else {
+            console.log('Webhook is already set');
+        }
+    } catch (error) {
+        console.error('Error checking or setting webhook:', error);
+    }
+};
+
