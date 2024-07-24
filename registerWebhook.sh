40ef7862935d951782d306b69826c507e08d2a24
Liu ChaoChih <chaochihliu@gmail.com>
2024-07-24 15:42:55 +0800

    refactor

--- registerWebhook.sh
+++ registerWebhook.sh
@@ -18,7 +18,7 @@ done < "$PROPERTIES_FILE"
 echo $TELEGRAM_BOT_TOKEN
 echo $WEBHOOK_HOST
 
-cmd="curl -F 'url=${WEBHOOK_HOST}' -F 'certificate=@/etc/pki/nginx/telegram/server.crt' 'https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/setWebhook'"
+cmd="curl -F 'curl=$WEBHOOK_HOST' -F 'certificate=@/etc/pki/nginx/telegram/server.crt' 'https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/setWebhook'"
 echo "$cmd"
 eval "$cmd"
 cmd="curl -X GET 'https://api.telegram.org/bot$TELEGRAM_BOT_TOKEN/getWebhookInfo'"
