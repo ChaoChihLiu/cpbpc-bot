74fca056d9528ec07b911cee7ee6e58c47aa97b6
Liu ChaoChih <chaochihliu@gmail.com>
2024-08-10 12:00:17 +0800

    split entire article into paragraphs, and match paragraphs with nlp

--- src/service/openAI.mjs
+++ src/service/openAI.mjs
@@ -135,8 +135,8 @@ function rephraseKeywordQuestion(question) {
 }
 
 function rephraseSynonymQuestion(question) {
-    return `Comprehend the meaning of the following question, provide keywords and key phrases to question and provide synonyms as many as possible: ${question}
-                and follow this pattern '{"keywords":["key word", "key phrase"...], "synonyms":["synonym1", "synonym2",...]}' to organise your response`;
+    return `Comprehend the meaning of the following question, provide keywords and key phrases to question, as well as rephrasing my question and provide synonyms as many as possible: ${question}
+                and follow this pattern '{"keywords":["key word", "key phrase"...], "synonyms":["synonym", "rephrasing question",...]}' to organise your response`;
     // return `Comprehend the meaning of the following question, and provide synonyms as many as possible, sort synonyms by the relevance level to the context of the question:
     //             ${question}
     //             and follow this pattern '["synonym1", "synonym2", "synonym3"...]' to organise your response`;
