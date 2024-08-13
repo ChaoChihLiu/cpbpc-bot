68a0ec7d422860f9d75d47c4cfbea710adca3764
Liu ChaoChih <chaochihliu@gmail.com>
2024-08-13 23:15:37 +0800

    change question to openAI

--- src/service/openAI.mjs
+++ src/service/openAI.mjs
@@ -135,8 +135,8 @@ function rephraseKeywordQuestion(question) {
 }
 
 function rephraseSynonymQuestion(question) {
-    return `Comprehend the meaning of the following question, provide keywords and key phrases to question, as well as synonyms as many as possible, but don't help me rephrase question : ${question}
-                and follow this pattern '{"keywords":["key word", "key phrase"...], "synonyms":["synonym1", "synonym2",...]}' to organise your response`;
+    return `Comprehend the meaning of the following question, provide keywords and key phrases to question, as well as synonyms as relevant as possible, but don't help me rephrase question : ${question}
+                and follow this pattern '{"keywords":["keyword", "key phrase"...], "synonyms":["synonym1", "synonym2",...]}' to organise your response`;
     // return `Comprehend the meaning of the following question, and provide synonyms as many as possible, sort synonyms by the relevance level to the context of the question:
     //             ${question}
     //             and follow this pattern '["synonym1", "synonym2", "synonym3"...]' to organise your response`;
