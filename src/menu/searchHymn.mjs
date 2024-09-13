359af552f272d4418c414a4aedb0cab6f3e9e18a
Liu ChaoChih <chaochihliu@gmail.com>
2024-09-13 16:22:11 +0800

    clean state

--- src/menu/searchHymn.mjs
+++ src/menu/searchHymn.mjs
@@ -2,6 +2,7 @@ import {cleanState, hashHeader, keepState, WAIT_FOR_INPUT} from "../userstat.mjs
 import _ from 'lodash';
 import env from 'dotenv';
 import decimal from 'decimal.js'
+import {ListObjectsV2Command, S3Client} from '@aws-sdk/client-s3'
 
 env.config();
 
@@ -20,13 +21,43 @@ export async function handleWaitForInput(msg) {
     const hymnNum = _.toLower(msg.text)
     const userstat_key = hashHeader(msg.from);
     cleanState(userstat_key);
+
+    let isExisted = await searchS3Objects( 'testhymn', hymnNum, '.jpg' )
+    if( !isExisted ){
+        return { text: `Hymn number ${hymnNum} not exist` };
+    }
     return { text: `https://d13vhl06g9ql7i.cloudfront.net/hymn/testhymn/num/${hymnNum}` };
 }
 
-function splitArray(arr, size) {
-    const result = [];
-    for (let i = 0; i < arr.length; i += size) {
-        result.push(arr.slice(i, i + size));
-    }
-    return result;
+const s3 = new S3Client({ region: 'ap-southeast-1' });
+async function searchS3Objects(bucketName, prefix, postfix) {
+    let continuationToken = null;
+
+    do {
+        const params = {
+            Bucket: bucketName,
+            Prefix: `${prefix}_`,
+            ContinuationToken: continuationToken
+        };
+
+        try {
+            const data = await s3.send(new ListObjectsV2Command(params));
+
+            // Check if any object keys match the prefix and postfix criteria
+            for (const object of data.Contents) {
+                if (object.Key.startsWith(`${prefix}_`) && object.Key.endsWith(postfix)) {
+                    return true; 
+                }
+            }
+
+            continuationToken = data.IsTruncated ? data.NextContinuationToken : null;
+
+        } catch (err) {
+            console.error('Error listing objects:', err);
+            return false; // Return false on error
+        }
+    } while (continuationToken);
+
+    return false; 
 }
+
